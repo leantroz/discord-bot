d4fee41ba276993d58ae19820ea5d54e9f6ea64a
Leandro <leantroz@gmail.com>
2026-05-03 15:13:43 -0300

    hotfix

--- resetcommands.js
+++ resetcommands.js
@@ -1,4 +1,17 @@
-await rest.put(
-  Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
-  { body: [] }
-);
+require('dotenv').config();
+const { REST, Routes } = require('discord.js');
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log('Reseteando comandos de guild...');
+    await rest.put(
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      { body: [] } // ✅ esto borra todos los comandos de ese servidor
+    );
+    console.log('Comandos de guild reseteados.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
